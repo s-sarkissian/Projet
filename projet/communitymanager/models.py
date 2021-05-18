757f81a19aed44c1134298371bcb4c607466c1ac
s-sarkissian <sevan.sarkissian@student.isae-supaero.fr>
2021-05-18 11:26:00 +0200

    added user authentication, test user and community model

--- projet/communitymanager/models.py
+++ projet/communitymanager/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Community(models.Model):
+    name = models.CharField(max_length=200)
+    subscribers = models.ManyToManyField(User)
