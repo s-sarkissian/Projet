a6e01936a376df718ee69a96143f8890fc178be5
s-sarkissian <sevan.sarkissian@student.isae-supaero.fr>
2021-05-18 11:49:52 +0200

    added communities path

--- projet/communitymanager/views.py
+++ projet/communitymanager/views.py
@@ -1,6 +1,7 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-
 # Create your views here.
-def community(request):
-    return render(request, 'communitymanager/communities.html')
+def communaute(request):
+    @login_required()
+    return render(request, 'communitymanager/communautes.html')
