1e81aa6cecbbc6832eb204994a9660c826324387
s-sarkissian <sevan.sarkissian@student.isae-supaero.fr>
2021-05-25 12:08:09 +0200

    added comment model and administration

--- projet/communitymanager/admin.py
+++ projet/communitymanager/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Community, Post, Priorite
+from .models import Community, Post, Priorite, Comment
 
 # Register your models here.
 admin.site.register(Community)
 admin.site.register(Post)
 admin.site.register(Priorite)
+admin.site.register(Comment)
--- projet/communitymanager/models.py
+++ projet/communitymanager/models.py
@@ -6,22 +6,37 @@ from django.db import models
 class Community(models.Model):
     name = models.CharField(max_length=200)
     subscribers = models.ManyToManyField(User)
+
     def __str__(self):
         return self.name
 
+
 class Priorite(models.Model):
     label = models.CharField(max_length=200)
+
     def __str__(self):
         return self.label
+
 
 class Post(models.Model):
     titre = models.CharField(max_length=200)
     description = models.TextField()
     date_creation = models.DateTimeField()
-    community = models.ForeignKey(Community, on_delete='SET_NULL')
+    community = models.ForeignKey(Community, on_delete='CASCADE')
     priorite = models.ForeignKey(Priorite, on_delete='CASCADE')
     evenementiel = models.BooleanField()
     date_evenement = models.DateField()
     auteur = models.ForeignKey(User, on_delete='CASCADE')
+
     def __str__(self):
         return self.titre
+
+
+class Comment(models.Model):
+    date_creation = models.DateTimeField()
+    contenu = models.TextField()
+    auteur = models.ForeignKey(User, on_delete='CASCADE')
+    post = models.ForeignKey(Post, on_delete='CASCADE')
+
+    def __str__(self):
+        return self.auteur.username
